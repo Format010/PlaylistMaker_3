c9fd4e1dcfd3bf349aa060bcaa301671d7ad3f07
Format010 <foreneng@yandex.ru>
2024-02-02 15:56:05 +0300

    Com6

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,15 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         val back = findViewById<View>(R.id.back)
+        val share = findViewById<View>(R.id.buttonShare)
+
+        share.setOnClickListener{
+            val shareApp = Intent()
+            shareApp.action = Intent.ACTION_SEND
+            shareApp.type = "text/plain"
+            shareApp.putExtra(Intent.EXTRA_TEXT, "Воспользуйся курсом от https://practicum.yandex.ru/learn/android-developer")
+            startActivity(Intent.createChooser(shareApp, getString(R.string.share)))
+        }
 
         back.setOnClickListener{
             onBackPressed()
