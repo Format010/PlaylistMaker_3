e42e3b73fc77556c230aaca6fa9ca1a31f54325b
Format010 <foreneng@yandex.ru>
2024-02-06 09:11:37 +0300

    Com7

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -13,28 +13,35 @@ class SettingsActivity : AppCompatActivity() {
         val back = findViewById<View>(R.id.back)
         val share = findViewById<View>(R.id.buttonShare)
         val support = findViewById<View>(R.id.buttonMailToSupport)
+        val userAgreement = findViewById<View>(R.id.buttonUserAgr)
 
         share.setOnClickListener{
+            val textShare = getString(R.string.messageShare)
             val shareApp = Intent()
             shareApp.action = Intent.ACTION_SEND
             shareApp.type = "text/plain"
-            shareApp.putExtra(Intent.EXTRA_TEXT, "Воспользуйся курсом от https://practicum.yandex.ru/learn/android-developer")
-            startActivity(Intent.createChooser(shareApp, getString(R.string.share)))
+            shareApp.putExtra(Intent.EXTRA_TEXT, textShare)
+            startActivity(Intent.createChooser(shareApp, getString(R.string.shareWith)))
         }
 
         back.setOnClickListener{
             onBackPressed()
         }
         support.setOnClickListener{
-            val subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
-            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
-
+            val subject = getString(R.string.subjectMailto)
+            val message = getString(R.string.bodyMailto)
+            val email = getString(R.string.emailMailto)
             val supportIntent = Intent(Intent.ACTION_SENDTO)
             supportIntent.data = Uri.parse("mailto:")
-            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("foreneng@yandex.ru"))
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
             supportIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
             supportIntent.putExtra(Intent.EXTRA_TEXT, message)
             startActivity(supportIntent)
+        }
+        userAgreement.setOnClickListener{
+            val agreement = getString(R.string.userAgreement)
+            val userAgreementUrl = Intent(Intent.ACTION_VIEW, Uri.parse(agreement))
+            startActivity(userAgreementUrl)
         }
     }
 }
